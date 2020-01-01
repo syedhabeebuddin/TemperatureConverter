b5f63e64778c546d63039425e7d804a83da57da8
syedhabeebuddin <syedhabeebuddin0@gmail.com>
2020-01-01 21:45:21 +0530

    Updates

---
+++ precache-manifest.ab5584f1e902e88f55fb4c085952be04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b678e7de8d237a843f0d52a9ebaf1135",
+    "url": "/TemperatureConverter/index.html"
+  },
+  {
+    "revision": "235ef0a43468f76aec77",
+    "url": "/TemperatureConverter/static/css/main.0afdf6b9.chunk.css"
+  },
+  {
+    "revision": "a79443c75c7c369127dd",
+    "url": "/TemperatureConverter/static/js/2.e8513ea4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TemperatureConverter/static/js/2.e8513ea4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "235ef0a43468f76aec77",
+    "url": "/TemperatureConverter/static/js/main.27d0dacb.chunk.js"
+  },
+  {
+    "revision": "3a54dad58864d10f64ca",
+    "url": "/TemperatureConverter/static/js/runtime-main.624b03b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TemperatureConverter/static/media/logo.5d5d9eef.svg"
+  }
+]);
